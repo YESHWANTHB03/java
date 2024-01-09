be072adf47332c970b70aab0bf25dfe14120d52c
YESHWANTHB03 <124280153+YESHWANTHB03@users.noreply.github.com>
2024-01-09 10:43:33 +0530

    Update yesh.java

--- yesh.java
+++ yesh.java
@@ -1,7 +1,5 @@
-class yesh 
-{
-    public static void main(String[] args) 
-    {
+public class yesh {
+    public static void main(String[] args) {
         System.out.println("Hello, World!"); 
     }
 }
